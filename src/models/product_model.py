62afd062746cae7e5ceb3c6da16f0bd72164df36
josue-avila <josue.avila@olist.com>
2021-02-05 13:54:05 -0300

    adding sqlalchemy @validates to product's box attributes

--- src/models/product_model.py
+++ src/models/product_model.py
@@ -13,7 +13,8 @@ class Product(BaseModel):
     length = Column('box_length', Float, nullable=True)
     weight = Column('box_weight', Float, nullable=True)
 
-    def __init__(self, name: str, price: float, height: float, width: float, length: float, weight: float, description: str = None) -> None:
+    def __init__(self, name: str, price: float, height: float, width: float, length: float, weight: float,
+                 description: str = None) -> None:
         self.name = name
         self.price = price
         self.height = height
@@ -21,3 +22,35 @@ class Product(BaseModel):
         self.length = length
         self.weight = weight
         self.description = description
+
+    @validates('height')
+    def validates_height(self, key, height: float) -> float:
+        if not isinstance(height, float):
+            raise TypeError('Box height must be float')
+        if 1.0 > height > 100.0:
+            raise ValueError('Box height must be between 1.0cm and 100.0cm')
+        return height
+
+    @validates('width')
+    def validates_width(self, key, width: float) -> float:
+        if not isinstance(width, float):
+            raise TypeError('Box width must be float')
+        if 10.0 > width > 100.0:
+            raise ValueError('Box height must be between 10.0cm and 100.0cm')
+        return width
+
+    @validates('length')
+    def validates_length(self, key, length: float) -> float:
+        if not isinstance(length, float):
+            raise TypeError('Box length must be float')
+        if 15.0 > length > 100.0:
+            raise ValueError('Box length must be between 15.0cm and 100.0cm')
+        return length
+
+    @validates('weight')
+    def validates_length(self, key, weight: float) -> float:
+        if not isinstance(weight, float):
+            raise TypeError('Box weight must be float')
+        if 0.0 > weight > 30.0:
+            raise ValueError('Box weight must be between 0.0Kg and 30Kg')
+        return weight
