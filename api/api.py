c95b258ea8a4b3b69f201974149a2e457b2ef0ec
josue-avila <josue.avila@olist.com>
2021-02-05 21:47:48 -0300

    fixing issue on api url, chanding <int:id> to <int:id_> to match resources modules

--- api/api.py
+++ api/api.py
@@ -6,11 +6,10 @@ from src.resources.product_resources import ProductResource
 app_api = Flask(__name__)
 api = Api(app_api)
 
-api.add_resource(ProductResource, '/api/product', endpoint='produc')
-api.add_resource(ProductResource, '/api/product/<int:id>', endpoint='product')
+api.add_resource(ProductResource, '/api/product', endpoint='products')
+api.add_resource(ProductResource, '/api/product/<int:id_>', endpoint='product')
 
 
 @app_api.route('/')
 def index():
     return 'Bem Vindo a API do olist!'
-    
